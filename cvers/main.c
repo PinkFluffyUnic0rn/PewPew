d02adf807edf6ce53e1fe4fed2d23d34eb7debf9
Evgeniy <qwerty@localhost.localdomain>
2015-08-24 05:28:05 +0300

    Another stupid commit (cvers)
    shot sound added
    perspective transform fixed (i think so)
    checking shots for hit
    generating new aim position after every hit

--- cvers/main.c
+++ cvers/main.c
@@ -3,29 +3,40 @@
 #include <math.h>
 #include <cv.h>
 #include <highgui.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h> 
 
 #define STATE_SHOT 1
 #define STATE_WAIT 0
 
-#define ARROW_LEFT 	65361
-#define ARROW_UP 	65362
-#define ARROW_RIGHT	65363
-#define ARROW_DOWN 	65364
-#define ESC		27
-#define SPACE		32
-#define KEY_P		112
+enum KEY {	
+	ARROW_LEFT	= 65361,
+	ARROW_UP	= 65362,
+	ARROW_RIGHT	= 65363,
+	ARROW_DOWN 	= 65364,
+	ESC		= 27,
+	SPACE		= 32,
+	KEY_P		= 112
+};
 
-struct Aim
-{
+struct Aim {
 	IplImage *img;
 	CvPoint pos;
 };
 
-struct Perspquad
-{
+struct Perspquad {
 	CvPoint2D32f points[4];
 	int pointc;
 };
+
+void init_allegro()
+{
+	al_init();
+	al_install_audio();
+	al_init_acodec_addon();
+	al_reserve_samples(10);
+}
 
 //Gets Value from HSV
 unsigned char *getvals(IplImage *img) 
@@ -106,13 +117,11 @@ void cleartbl(IplImage *table, int v)
 	data = (unsigned char*)table->imageData;
 
 		
-	for (y = 0; y < h; y++) {
-		for (x = 0; x < w; x++) {
-			for (i = 0; i < chans; i++) { 
+	for (y = 0; y < h; y++)
+		for (x = 0; x < w; x++)
+			for (i = 0; i < chans; i++)
 				data[chans * (y * w + x) + i] = v;
-			}
-		}
-	}
+
 }
 
 void drawaim(struct Aim *aim, IplImage *table)
@@ -129,13 +138,10 @@ void drawaim(struct Aim *aim, IplImage *table)
 	adata = (unsigned char*)aim->img->imageData;
 	tdata = (unsigned char*)table->imageData;
 
-	for (y = 0; y < h; y++) {
-		for (x = 0; x < w; x++) {
-			for (i = 0; i < chans; i++) { 
+	for (y = 0; y < h; y++)
+		for (x = 0; x < aim->img->width; x++)
+			for (i = 0; i < chans; i++) 
 				tdata[chans * ((y + oy) * tw + x + ox) + i] = adata[chans * (y * w + x) + i];
-			}
-		}
-	}
 }
 
 void mouseclick(int e, int x, int y, int a, void *arg)
@@ -175,20 +181,37 @@ IplImage *persp(IplImage *in, struct Perspquad *perspquad, int w, int h)
 		perspquad->points[2] = perspquad->points[3];
 		perspquad->points[3] = tmp;	
 	}
-
-	outp[0] = cvPoint2D32f(in->height * w / h, in->height);
-	outp[2] = cvPoint2D32f(in->height * w / h, 0);
-	outp[1] = cvPoint2D32f(0, in->height);
+	
+	outp[0] = cvPoint2D32f(in->width, in->width * h / w);
+	outp[2] = cvPoint2D32f(in->width, 0);
+	outp[1] = cvPoint2D32f(0, in->width * h / w);
 	outp[3] = cvPoint2D32f(0, 0);
 
 	cvGetPerspectiveTransform(perspquad->points, outp, mpersp);
 
 	cvWarpPerspective(in, copy, mpersp,
 		CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS, cvScalarAll(0));
+	
+	cvReleaseMat(&mpersp);
 
 	return copy;
 }
 
+int ishit(CvPoint *shot, CvPoint *aimpos, int w, int h, int tw, int th, int d)
+{
+/*
+	int x = shot->x * tw / w;
+	int y = shot->y * th / h;
+*/
+	int x = shot->x;
+	int y = shot->y;
+
+
+	int dx = x - aimpos->x;
+	int dy = y - aimpos->y;
+	
+	return (dx * dx + dy * dy) < (d * d);
+}
 int main(int argc, char **argv)
 {
 	int dev, flag, maxshots, nshots, state;
@@ -198,6 +221,7 @@ int main(int argc, char **argv)
 	struct Aim aim;
 	IplImage *table;
 	struct Perspquad perspquad;
+	ALLEGRO_SAMPLE *shot_sound;
 
 	dev = 0;
 	if (argc > 1)
@@ -205,6 +229,8 @@ int main(int argc, char **argv)
 
 	capt = cvCaptureFromCAM(dev);
 
+	init_allegro();
+	shot_sound = al_load_sample("shot.wav");
 
 	flag = 1;
 	maxshots = 1024;
@@ -216,8 +242,8 @@ int main(int argc, char **argv)
 
 	aim.img = cvLoadImage("aim.png", CV_LOAD_IMAGE_COLOR);
 	printf("w=%d h=%d chans=%d\n", aim.img->width, aim.img->height, aim.img->nChannels);
-	aim.pos.x = 100;
-	aim.pos.y = 0;
+	aim.pos.x = 200;
+	aim.pos.y = 100;
 	table = cvCreateImage(cvSize(tw, th), aim.img->depth, aim.img->nChannels);
 
 	perspquad.pointc = 0;
@@ -232,17 +258,18 @@ int main(int argc, char **argv)
 		int key, i, n;
 		unsigned char *vdata;
 		CvPoint shot;
-		IplImage *frame, *copy;
+		IplImage *frame, *copy, *flat;
 	
 		frame = cvQueryFrame(capt);	
+		flat = cvCloneImage(frame);
 		
 		if (perspquad.pointc == 4) {
-			copy = persp(frame, &perspquad, tw, th);
-			frame = cvCloneImage(copy);	
-		}
-		
+			copy = persp(flat, &perspquad, tw, th);
+			cvReleaseImage(&flat);
+			flat = copy;
+		}	
 			
-		copy = cvCloneImage(frame);	
+		copy = cvCloneImage(flat);	
 
 
 		cleartbl(table, 128);
@@ -251,13 +278,31 @@ int main(int argc, char **argv)
 
 		vdata = getvals(copy);
 		n = getshot(copy, vdata, &shot);
+		free(vdata);
+
 		if (n > 0 && state == STATE_WAIT) {
+			al_play_sample(shot_sound, 1.0f, 0.0f, 1.0f,
+				ALLEGRO_PLAYMODE_ONCE, NULL);
+			
 			shots[nshots] = shot;
 			state = STATE_SHOT;
 			nshots++;
+
 			if (nshots == maxshots) {
 				maxshots *= 2;
 				shots = realloc(shots, sizeof(CvPoint) * maxshots);
+			}
+
+			if (ishit(&shot, &(aim.pos),
+				flat->width, flat->height, tw, th, 15)) {
+				
+				/*
+				aim.pos.x = rand() % tw;
+				aim.pos.y = rand() % th;
+				*/
+				aim.pos.x = rand() % flat->width;
+				aim.pos.y = rand() % flat->height;
+
 			}
 		} else if (n == 0) {
 			state = STATE_WAIT;
@@ -266,6 +311,8 @@ int main(int argc, char **argv)
 
 		for (i = 0; i < nshots; i++)
 			cvCircle(copy, shots[i], 5, CV_RGB(0, 255, 0), 2, 8, 0);
+		
+		cvCircle(copy, aim.pos, 5, CV_RGB(255, 0, 0), 2, 8, 0);
 
 		if (perspquad.pointc > 1 && perspquad.pointc < 4)
 			for (i = 0; i < perspquad.pointc; ++i) {
@@ -276,19 +323,22 @@ int main(int argc, char **argv)
 				p2 = cvPoint(perspquad.points[i].x,
 					perspquad.points[i].y); 
 
-				cvLine(frame, p1, p2, CV_RGB(0, 255, 0), 2, 8, 0);
+				cvLine(flat, p1, p2, CV_RGB(0, 255, 0), 2, 8, 0);
 			}
 
-		cvShowImage("Flat", frame);
+		cvShowImage("Flat", flat);
 		cvShowImage("Blackened", copy);
 
-		key = cvWaitKey(10);
+		cvReleaseImage(&copy);
+		cvReleaseImage(&flat);
 
-		switch (key) {
+		switch ((key = cvWaitKey(10))) {
 		case ESC:
 			flag = 0;
 			break;
-
+		case KEY_P:
+			perspquad.pointc = 0;
+			break;
 		default:
 			if (key != -1)
 				printf("keycode=%d\n", key);;
